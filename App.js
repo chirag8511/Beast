eb6b390a00ec963b377f1466ffa0b9f29d972a3f
chirag8511 <Thakorchirag239@gmail.com>
2025-04-13 02:29:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Beast Mode Dashboard</h1>
+      <p>Let’s turn that 10k into 1L. Stay focused. Trade smart.</p>
+    </div>
+  );
+}
+
+export default App;
